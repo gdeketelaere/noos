a6c6173455ea917cb38140a1098efc76338435a7
Deketelaere <gdk@emakina.com>
2020-06-05 12:53:55 +0200

    Header Practionner cleaning

--- Camp/directory/front-end/templates/doctors/single/basic.php
+++ Camp/directory/front-end/templates/doctors/single/basic.php
@@ -1,168 +1,174 @@
-<?php
-/**
- *
- * The template used for doctors basics
- *
- * @package   Doctreat
- * @author    amentotech
- * @link      https://amentotech.com/user/amentotech/portfolio
- * @version 1.0
- * @since 1.0
- */
-
-global $post,$theme_settings;
-$post_id 	= $post->ID; 
-$user_id	= doctreat_get_linked_profile_id( $post_id,'post' );
-$verified	= get_post_meta($post_id, '_is_verified', true);
-$verified	= !empty( $verified ) ? $verified	: '';
-
-$system_access	= !empty($theme_settings['system_access']) ? $theme_settings['system_access'] : '';
-
-$shoert_des		= doctreat_get_post_meta( $post_id, 'am_short_description');
-$tagline		= doctreat_get_post_meta( $post_id, 'am_sub_heading');
-$mrv			= doctreat_get_post_meta( $post_id, 'am_registration_number');
-$name			= doctreat_full_name( $post_id );
-$name			= !empty( $name ) ? $name : '';
-
-$booking_option	= doctreat_get_booking_oncall_option('oncall');
-
-$feedback			= get_post_meta($post_id,'review_data',true);
-$feedback			= !empty( $feedback ) ? $feedback : array();
-$total_rating		= !empty( $feedback['dc_total_rating'] ) ? $feedback['dc_total_rating'] : 0 ;
-$total_percentage	= !empty( $feedback['dc_total_percentage'] ) ? $feedback['dc_total_percentage'] : 0 ;
-
-$doctor_avatar = apply_filters(
-					'doctreat_doctor_avatar_fallback', doctreat_get_doctor_avatar( array( 'width' => 255, 'height' => 250 ), $post_id ), array( 'width' => 255, 'height' => 250 )
-				);
-
-$doctor_avatar_2x = apply_filters(
-					'doctreat_doctor_avatar_fallback', doctreat_get_doctor_avatar( array( 'width' => 545, 'height' => 428 ), $post_id ), array( 'width' => 545, 'height' => 428 )
-				);
-
-$featured	= get_post_meta($post_id,'is_featured',true);
-
-
-//Ajout Cyril K
-
-$address		= get_post_meta( $post->ID , '_address',true );
-$address		= !empty( $address ) ? $address : 'Paris';
-$map = do_shortcode( '[camp_map]' );
-//$map ='<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11376654.143867694!2d-6.932249111537538!3d45.881243114560725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54a02933785731%3A0x6bfd3f96c747d9f7!2sFrance!5e0!3m2!1sfr!2sfr!4v1588783881376!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
-
-
-
-?>
-<div class="dc-docsingle-header">
-	<?php if( !empty( $doctor_avatar ) ){?>
-		<figure class="dc-docsingleimg">
-			<img class="dc-ava-detail" src="<?php echo esc_url( $doctor_avatar );?>" alt="<?php echo esc_attr( get_the_title() );?>">
-			<img class="dc-ava-detail-2x" src="<?php echo esc_url( $doctor_avatar_2x );?>" alt="<?php echo esc_attr( get_the_title() );?>">
-			<?php if( !empty( $featured ) && intval($featured) > 0 ){ ?>
-				<figcaption>
-					<span class="dc-featuredtag"><i class="fa fa-bolt"></i></span>
-				</figcaption>
-			<?php } ?>
-		</figure>
-	<?php }?>
-	<div class="dc-docsingle-content">
-		<div class="dc-title">
-			<h2>
-				<a href="<?php echo esc_url( get_the_permalink() );?>"><?php echo esc_html( $name );?></a>
-				<?php do_action('doctreat_get_drverification_check',$post_id,esc_html__('Education Verified','doctreat'));?>
-				<?php do_action('doctreat_get_verification_check',$post_id,'');?>
-			</h2>
-			<div class="dc-docinfo">				
-				<div><?php echo $map ;?></div>					
-				<div>
-					<i class="lnr lnr-location"></i><?php echo $address ;?>
-				</div>				
-			</div>
-			<?php do_action('camp_specilities_list',$post_id);?>
-			<?php do_action('camp_specilities_list',$post_id,'symtomes');?>			
-		</div>
-		<div class="dc-btnarea">
-			<a href="#" class="dc-btn" id ="myBtn_contact"><?php _e('Contact','doctreat');?></a>
-				<?php if(empty($booking_option)){?>
-					<a href="javascript:;" class="dc-btn dc-booking-model" data-access="<?php echo esc_attr($system_access);?>"><?php _e('Book Now','doctreat');?></a>
-				<?php } else { ?>
-					<a href="javascript:;" data-id="<?php echo intval( $post_id );?>" class="dc-btn dc-booking-contacts"><?php _e('Call Now','doctreat');?></a>
-				<?php } ?>
-				
-			<?php do_action('camp_get_favorit_check',$post_id,'large');?>
-		</div>
-		<div class="dc-btnarea">
-			<?php doctreat_prepare_social_sharing( false,esc_html__('Share Profile','doctreat'),true,'dc-simplesocialicons dc-socialiconsborder',$thumbnail ); ?>
-		</div>
-	</div>
-</div>
-<div id="myModal" class="modal">
-    <!-- Modal content -->
-	<div class="modal-content">
-	    <div class="modal-header">
-	        <span class="close">×</span>
-	      
-			<div class="container">
-				<div class="dc-tabscontenttitle dc-addnew">
-					<h3><?php esc_html_e('Contact ','doctreat'); echo $name; ?></h3>
-				</div>					
-				<div class="dc-collapseexp" style="background-color: transparent;">
-					<div class="dc-formtheme dc-userform">
-						<fieldset>
-							<form action="" method="post">
-		    					<?php wp_nonce_field( "action_camp_contact_doctors", "camp_contact_doctors", true, true );?>
-								
-								<div class="form-group form-group-half">
-									<input type="text" class="form-control" name="camp_contact_nom_doctors" class="" placeholder="<?php esc_attr_e('Nom','doctreat');?>" value="<?php echo($_POST['camp_contact_nom_doctors']);?>">
-								</div>
-								<div class="form-group form-group-half">
-									<input type="text" class="form-control" name="camp_contact_prenom_doctors" class="" placeholder="<?php esc_attr_e('Prénom','doctreat');?>" value="<?php echo($_POST['camp_contact_prenom_doctors']);?>">
-								</div>
-								<div class="form-group form-group-half">
-									<input type="email" class="form-control" name="camp_contact_email_doctors" class="" placeholder="<?php esc_attr_e('E-mail','doctreat');?>" value="<?php echo($_POST['camp_contact_email_doctors']);?>">
-								</div>							
-								<div class="form-group form-group-half">
-									<input type="tel" class="form-control" name="camp_contact_tel_doctors" class="" placeholder="<?php esc_attr_e('Téléphone','doctreat');?>" value="<?php echo($_POST['camp_contact_tel_doctors']);?>">
-								</div>
-								
-								<div class="form-group">
-									<textarea name="" class="form-control" name="camp_contact_message_doctors" placeholder="<?php esc_attr_e('Votre message ...','doctreat');?>"><?php echo($_POST['camp_contact_message_doctors']);?></textarea>
-								</div>
-								<div class="form-group">
-									<input type= "hidden" name="camp_contact_id_doctors" value="<?php echo $post_id ;?>">			
-									<input type= "submit" class="add-repeater-services dc-btn" style="border" value="<?php esc_attr_e('Envoyer','doctreat');?>">
-								</div>
-							</form>								
-						</fieldset>
-					</div> 						  
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<script>
-	// Get the modal
-	var modal = document.getElementById('myModal');
-
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn_contact");
-
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-	  modal.style.display = "block";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	  modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	  if (event.target == modal) {
-	    modal.style.display = "none";
-	  }
-	}
-</script>
+<?php
+
+/**
+
+ *
+
+ * The template used for doctors basics
+
+ *
+
+ * @package   Doctreat
+
+ * @author    amentotech
+
+ * @link      https://amentotech.com/user/amentotech/portfolio
+
+ * @version 1.0
+
+ * @since 1.0
+
+ */
+
+
+
+global $post,$theme_settings;
+
+$post_id 	= $post->ID; 
+
+$user_id	= doctreat_get_linked_profile_id( $post_id,'post' );
+
+$verified	= get_post_meta($post_id, '_is_verified', true);
+
+$verified	= !empty( $verified ) ? $verified	: '';
+
+
+
+$system_access	= !empty($theme_settings['system_access']) ? $theme_settings['system_access'] : '';
+
+
+
+$shoert_des		= doctreat_get_post_meta( $post_id, 'am_short_description');
+
+$tagline		= doctreat_get_post_meta( $post_id, 'am_sub_heading');
+
+$mrv			= doctreat_get_post_meta( $post_id, 'am_registration_number');
+
+$name			= doctreat_full_name( $post_id );
+
+$name			= !empty( $name ) ? $name : '';
+
+
+
+$booking_option	= doctreat_get_booking_oncall_option('oncall');
+
+
+
+$feedback			= get_post_meta($post_id,'review_data',true);
+
+$feedback			= !empty( $feedback ) ? $feedback : array();
+
+$total_rating		= !empty( $feedback['dc_total_rating'] ) ? $feedback['dc_total_rating'] : 0 ;
+
+$total_percentage	= !empty( $feedback['dc_total_percentage'] ) ? $feedback['dc_total_percentage'] : 0 ;
+
+
+
+$doctor_avatar = apply_filters(
+
+					'doctreat_doctor_avatar_fallback', doctreat_get_doctor_avatar( array( 'width' => 255, 'height' => 250 ), $post_id ), array( 'width' => 255, 'height' => 250 )
+
+				);
+
+
+
+$doctor_avatar_2x = apply_filters(
+
+					'doctreat_doctor_avatar_fallback', doctreat_get_doctor_avatar( array( 'width' => 545, 'height' => 428 ), $post_id ), array( 'width' => 545, 'height' => 428 )
+
+				);
+
+
+
+$featured	= get_post_meta($post_id,'is_featured',true);
+
+
+
+
+
+//Ajout Cyril K
+
+
+
+$address		= get_post_meta( $post->ID , '_address',true );
+
+$address		= !empty( $address ) ? $address : 'Paris';
+
+$map = do_shortcode( '[camp_map]' );
+
+//$map ='<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11376654.143867694!2d-6.932249111537538!3d45.881243114560725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54a02933785731%3A0x6bfd3f96c747d9f7!2sFrance!5e0!3m2!1sfr!2sfr!4v1588783881376!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
+
+
+
+
+
+
+
+?>
+
+<div class="dc-docsingle-header">
+
+	<?php if( !empty( $doctor_avatar ) ){?>
+
+		<figure class="dc-docsingleimg">
+
+			<img class="dc-ava-detail" src="<?php echo esc_url( $doctor_avatar );?>" alt="<?php echo esc_attr( get_the_title() );?>">
+
+			<img class="dc-ava-detail-2x" src="<?php echo esc_url( $doctor_avatar_2x );?>" alt="<?php echo esc_attr( get_the_title() );?>">
+
+			<?php if( !empty( $featured ) && intval($featured) > 0 ){ ?>
+
+				<figcaption>
+
+					<span class="dc-featuredtag"><i class="fa fa-bolt"></i></span>
+
+				</figcaption>
+
+			<?php } ?>
+
+		</figure>
+
+	<?php }?>
+
+	<div class="dc-docsingle-content">
+
+		<div class="dc-title">
+
+			<h2>
+
+				<a href="<?php echo esc_url( get_the_permalink() );?>"><?php echo esc_html( $name );?></a>
+
+				<?php do_action('doctreat_get_drverification_check',$post_id,esc_html__('Education Verified','doctreat'));?>
+
+				<?php do_action('doctreat_get_verification_check',$post_id,'');?>
+
+			</h2>
+
+			<div class="dc-docinfo">				
+
+				<div><?php echo $map ;?></div>					
+
+				<div class="dc-docinfo-location">
+
+					<img class="dc-icon--small" src="<?php echo get_stylesheet_directory_uri() . '/assets/svg/map-pointer.svg'; ?> "/><?php echo $address ;?>
+
+				</div>				
+
+			</div>
+
+			<div class="dc-docinfo-specialities">
+
+				<?php do_action('camp_specilities_list',$post_id);?>
+
+				<?php do_action('camp_specilities_list',$post_id,'symtomes');?>		
+
+			</div>
+		</div>
+
+
+		
+
+	</div>
+
+</div>
+
--- Camp/directory/front-end/templates/references.php
+++ Camp/directory/front-end/templates/references.php
@@ -1,119 +1,239 @@
-<?php
-/**
- *
- * The template part for displaying the dashboard menu
- *
- * @package   Doctreat
- * @author    Amentotech
- * @link      http://amentotech.com/
- * @since 1.0
- */
-
-
-global $post;
-$post_id 		 = $post->ID;
-$save_references_ids	= get_post_meta( $post_id, '_saved_references', true);
-$empty_array[] = array('id'=>0, 'comment'=>'');
-$array_ids=!empty($save_references_ids) ? $save_references_ids : $empty_array;
-$post_array_ids=array();
-
-foreach ($array_ids as $key => $value) {
-
-	$post_array_ids[]=$value['id'];
-
-}
-$show_posts 		= get_option('posts_per_page') ? get_option('posts_per_page') : 10;
-$pg_page 			= get_query_var('page') ? get_query_var('page') : 1; 
-$pg_paged 			= get_query_var('paged') ? get_query_var('paged') : 1;
-$paged 				= max($pg_page, $pg_paged);
-$order 	 			= 'DESC';
-$sorting 			= 'ID';
-$post_type=(string)get_post_type( $post_id );
-
-$args = array(
-	'posts_per_page' 	=> $show_posts,
-	'post_type' 		=> $post_type,
-	'orderby' 			=> $sorting,
-	'order' 			=> $order,
-	'post_status' 		=> $post_status,
-	'author' 			=> $user_identity,
-	'paged' 			=> $paged,
-	'post__in' 		    => $post_array_ids,
-    'suppress_filters' 	=> false
-);
-
-$loop = new WP_Query($args);
-$count_post = $loop->found_posts;
-?>
-
-<div class="dc-dashboardbox dc-dashboardinvocies">
-	<div class="dc-dashboardboxtitle dc-titlewithsearch">
-		<div class="dc-title"><h4><?php esc_html_e('My Network','doctreat');?></h4></div>
-	</div>
-
-	<div class="dc-dashboardboxcontent dc-categoriescontentholder dc-categoriesholder">		
-		<div class="dc-searchresult-grid dc-searchresult-list dc-searchvlistvtwo">
-			<?php
-
-				if($loop->have_posts()) :
-
-					while($loop->have_posts()) : $loop->the_post();
-					
-						
-
-						global $post;
-						$post_ref_id=$post->ID;
-						$featured	= get_post_meta(get_the_ID(),'is_featured',true);
-						$verified	= get_post_meta($post_ref_id, '_is_verified', true);
-						$verified	= !empty( $verified ) ? $verified	: '';
-						$address	= get_post_meta( get_the_ID() , '_address',true );
-						$address	= !empty( $address ) ? $address : 'Paris';
-				
-			?>
-						<div class="dc-docpostholder">
-							<div class="dc-docpostcontent" style="display: flex; flex-wrap: wrap; justify-content: space-between;">	
-								<div class="dc-searchvtwo" style="width: 80%;">								
-									<figure class="dc-docpostimg" style="width: 200px;">
-										<img class="dc-image-res" src="<?php the_post_thumbnail_url(); ?>" alt="Summer spring Hospital">
-										<img class="dc-image-res-2x" src="<?php the_post_thumbnail_url(); ?>" alt="Summer spring Hospital">
-									</figure>
-									<div class="dc-title">
-										<div class="dc-doc-specilities-tag">
-											<?php do_action('camp_specilities_list',$post_ref_id);?>
-											<?php do_action('camp_specilities_list',$post_ref_id,'symtomes');?>
-										</div>
-										<h3 style="width: 100%;">
-											<a href="<?php the_permalink();?>"><?php the_title();?></a>
-											<?php if (!empty($_is_verified)) {?>
-												<i class="far fa-check-circle dc-awardtooltip dc-tipso tipso_style" style="color:#1abc9c;" data-tipso="Verified user"></i><?php
-											}
-											?>												
-										</h3>
-										<div class="dc-docinfo" style="color:black;">
-											<?php echo camp_get_comment_reference ($post_id, get_the_ID()); ?>
-										</div>							
-									</div>
-								</div>
-								<div class="dc-actions" style="width: 20%;">
-									<a data-toggle="collapse" class="camp-check-reference" href="#">
-										<i class="lnr lnr-location"></i><?php echo $address ;?>
-									</a>										
-								</div>						
-							</div>
-						</div>	
-						<!-- The Modal -->			
-												
-					<?php endwhile;
-					wp_reset_postdata();
-					if (!empty($count_post) && $count_post > $show_posts) :
-						doctreat_prepare_pagination($count_post, $show_posts);
-					endif;
-				else: do_action('doctreat_empty_records_html','dc-empty-saved-doctors',esc_html__( 'No Recommandations found!', 'doctreat' ));
-				endif;
-			?>
-		</div>			
-	</div>
-</div>
-
-
-
+<?php
+
+/**
+
+ *
+
+ * The template part for displaying the dashboard menu
+
+ *
+
+ * @package   Doctreat
+
+ * @author    Amentotech
+
+ * @link      http://amentotech.com/
+
+ * @since 1.0
+
+ */
+
+
+
+
+
+global $post;
+
+$post_id 		 = $post->ID;
+
+$save_references_ids	= get_post_meta( $post_id, '_saved_references', true);
+
+$empty_array[] = array('id'=>0, 'comment'=>'');
+
+$array_ids=!empty($save_references_ids) ? $save_references_ids : $empty_array;
+
+$post_array_ids=array();
+
+
+
+foreach ($array_ids as $key => $value) {
+
+
+
+	$post_array_ids[]=$value['id'];
+
+
+
+}
+
+$show_posts 		= get_option('posts_per_page') ? get_option('posts_per_page') : 10;
+
+$pg_page 			= get_query_var('page') ? get_query_var('page') : 1; 
+
+$pg_paged 			= get_query_var('paged') ? get_query_var('paged') : 1;
+
+$paged 				= max($pg_page, $pg_paged);
+
+$order 	 			= 'DESC';
+
+$sorting 			= 'ID';
+
+$post_type=(string)get_post_type( $post_id );
+
+
+
+$args = array(
+
+	'posts_per_page' 	=> $show_posts,
+
+	'post_type' 		=> $post_type,
+
+	'orderby' 			=> $sorting,
+
+	'order' 			=> $order,
+
+	'post_status' 		=> $post_status,
+
+	'author' 			=> $user_identity,
+
+	'paged' 			=> $paged,
+
+	'post__in' 		    => $post_array_ids,
+
+    'suppress_filters' 	=> false
+
+);
+
+
+
+$loop = new WP_Query($args);
+
+$count_post = $loop->found_posts;
+
+?>
+
+
+
+<div class="dc-dashboardbox dc-dashboardinvocies">
+
+	<div class="dc-dashboardboxtitle dc-titlewithsearch">
+
+		<div class="dc-title"><h4><?php esc_html_e('My Network','doctreat');?></h4></div>
+
+	</div>
+
+
+
+	<div class="dc-dashboardboxcontent dc-categoriescontentholder dc-categoriesholder">		
+
+		<div class="dc-searchresult-grid dc-searchresult-list dc-searchvlistvtwo">
+
+			<?php
+
+
+
+				if($loop->have_posts()) :
+
+
+
+					while($loop->have_posts()) : $loop->the_post();
+
+					
+
+						
+
+
+
+						global $post;
+
+						$post_ref_id=$post->ID;
+
+						$featured	= get_post_meta(get_the_ID(),'is_featured',true);
+
+						$verified	= get_post_meta($post_ref_id, '_is_verified', true);
+
+						$verified	= !empty( $verified ) ? $verified	: '';
+
+						$address	= get_post_meta( get_the_ID() , '_address',true );
+
+						$address	= !empty( $address ) ? $address : 'Paris';
+
+				
+
+			?>
+
+						<div class="dc-docpostholder">
+
+							<div class="dc-docpostcontent" style="display: flex; flex-wrap: wrap; justify-content: space-between;">	
+
+								<div class="dc-searchvtwo" style="width: 80%;">								
+
+									<figure class="dc-docpostimg" style="width: 200px;">
+
+										<img class="dc-image-res" src="<?php the_post_thumbnail_url(); ?>" alt="Summer spring Hospital">
+
+										<img class="dc-image-res-2x" src="<?php the_post_thumbnail_url(); ?>" alt="Summer spring Hospital">
+
+									</figure>
+
+									<div class="dc-title">
+										
+											<div class="dc-doc-specilities-tag">
+
+												<?php do_action('camp_specilities_list',$post_ref_id);?>
+
+												<?php do_action('camp_specilities_list',$post_ref_id,'symtomes');?>
+
+											</div>
+										
+
+										<h3 style="width: 100%;">
+
+											<a href="<?php the_permalink();?>"><?php the_title();?></a>
+
+											<?php if (!empty($_is_verified)) {?>
+
+												<i class="far fa-check-circle dc-awardtooltip dc-tipso tipso_style" style="color:#1abc9c;" data-tipso="Verified user"></i><?php
+
+											}
+
+											?>												
+
+										</h3>
+
+										<div class="dc-docinfo" style="color:black;">
+
+											<?php echo camp_get_comment_reference ($post_id, get_the_ID()); ?>
+
+										</div>							
+
+									</div>
+
+								</div>
+
+								<div class="dc-actions" style="width: 20%;">
+
+									<a data-toggle="collapse" class="camp-check-reference" href="#">
+
+										<i class="lnr lnr-location"></i><?php echo $address ;?>
+
+									</a>										
+
+								</div>						
+
+							</div>
+
+						</div>	
+
+						<!-- The Modal -->			
+
+												
+
+					<?php endwhile;
+
+					wp_reset_postdata();
+
+					if (!empty($count_post) && $count_post > $show_posts) :
+
+						doctreat_prepare_pagination($count_post, $show_posts);
+
+					endif;
+
+				else: do_action('doctreat_empty_records_html','dc-empty-saved-doctors',esc_html__( 'No Recommandations found!', 'doctreat' ));
+
+				endif;
+
+			?>
+
+		</div>			
+
+	</div>
+
+</div>
+
+
+
+
+
+
+
